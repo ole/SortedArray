1cc41e4b8fddba7f170b436ec2a19f0207f1e551
Ole Begemann <ole@oleb.net>
2017-07-07 20:26:23 +0200

    Update Travis build script
    - Use `swift package clean` instead of the deprecated `swift build --clean`
    - Run Swift Package Manager tests in parallel
    - Print version and debug information before running
    - Cleanup

--- scripts/travis-build-script.sh
+++ scripts/travis-build-script.sh
@@ -4,17 +4,22 @@
 # <https://travis-ci.org/>. Supports testing Swift Package Manager
 # packages on macOS and Linux.
 
+set -ue -o pipefail
+
 echo "Running on OS: ${TRAVIS_OS_NAME}"
 
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
     # macOS
     # 1. Test using Swift Package Manager
-    swift build --clean
+    swift --version
+    swift package clean
     swift build
-    swift test
+    swift test --parallel
 
     # 2. Test using xcodebuild
-    set -o pipefail
+    xcodebuild -version
+    xcodebuild -showsdks
+    instruments -s devices
     xcodebuild test -scheme SortedArray-macOS | xcpretty
     xcodebuild test -scheme SortedArray-iOS -destination "platform=iOS Simulator,name=iPhone 7,OS=10.1" | xcpretty
     xcodebuild test -scheme SortedArray-tvOS -destination "platform=tvOS Simulator,name=Apple TV 1080p" | xcpretty
@@ -25,9 +30,9 @@ elif [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     echo "Using Docker image: ${DOCKER_IMAGE}"
     # Download the Docker container. This is not strictly necessary since
     # docker run would automatically download a missing container.
-    docker pull ${DOCKER_IMAGE}
+    docker pull "${DOCKER_IMAGE}"
     # Share the current directory (where Travis checked out the repository)
     # with the Docker container.
     # Then, in the container, cd into that directory and run the tests.
-    docker run --volume "$(pwd):/root/repo" ${DOCKER_IMAGE} /bin/bash -c "cd /root/repo; swift build --clean; swift build; swift test"
+    docker run --volume "$(pwd):/package" "${DOCKER_IMAGE}" /bin/bash -c "cd /package; swift --version; swift package clean; swift build; swift test --parallel"
 fi
