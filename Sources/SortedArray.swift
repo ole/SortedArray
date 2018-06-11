e4f96f11ee0d27f63a2c70fe6ada510751ce21bc
Klaas Pieter Annema <kpa@annema.me>
2018-06-11 14:44:17 +0200

    Silence nearly matching warning
    Swift 4.2 warns that the SortedArray returning version of filter nearly
    matches the one required by Sequence. The warning also helpfully tells
    us to move it to another extension.

--- Sources/SortedArray.swift
+++ Sources/SortedArray.swift
@@ -99,13 +99,15 @@ extension SortedArray: RandomAccessCollection {
     public subscript(position: Index) -> Element {
         return _elements[position]
     }
+}
 
-    /// Like `Sequence.filter(_:)`, but returns a `SortedArray` instead of an `Array`.
-    /// We can do this efficiently because filtering doesn't change the sort order.
-    public func filter(_ isIncluded: (Element) throws -> Bool) rethrows -> SortedArray<Element> {
-        let newElements = try _elements.filter(isIncluded)
-        return SortedArray(sorted: newElements, areInIncreasingOrder: areInIncreasingOrder)
-    }
+extension SortedArray {
+  /// Like `Sequence.filter(_:)`, but returns a `SortedArray` instead of an `Array`.
+  /// We can do this efficiently because filtering doesn't change the sort order.
+  public func filter(_ isIncluded: (Element) throws -> Bool) rethrows -> SortedArray<Element> {
+    let newElements = try _elements.filter(isIncluded)
+    return SortedArray(sorted: newElements, areInIncreasingOrder: areInIncreasingOrder)
+  }
 }
 
 extension SortedArray: CustomStringConvertible, CustomDebugStringConvertible {
