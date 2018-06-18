3a1d931d73357c6518a84b41829bda52fe4b4e58
Ole Begemann <ole@oleb.net>
2018-06-18 16:48:21 +0200

    Delete redundant #if check
    No longer needed now that we dropped Swift 3.

--- Tests/SortedArrayTests/SortedArrayTests.swift
+++ Tests/SortedArrayTests/SortedArrayTests.swift
@@ -12,11 +12,7 @@ class SortedArrayTests: XCTestCase {
 
     func testLinuxTestSuiteIncludesAllTests() {
         #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-            #if swift(>=4.0)
-                let darwinTestCount = SortedArrayTests.defaultTestSuite.testCaseCount
-            #else
-                let darwinTestCount = Int(SortedArrayTests.defaultTestSuite().testCaseCount)
-            #endif
+            let darwinTestCount = SortedArrayTests.defaultTestSuite.testCaseCount
             let linuxTestCount = SortedArrayTests.allTests.count
             XCTAssertEqual(linuxTestCount, darwinTestCount, "allTests (used for testing on Linux) is missing \(darwinTestCount - linuxTestCount) tests")
         #endif
